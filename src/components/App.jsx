81a8b3cc75effcedc79a4dd570d58e0cada541b1
Seth Lieberman <seth.lieberman@powerhrg.com>
2016-08-19 13:36:32 -0400

    env set up and search bar created

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+
+import SeachBar from '../container/search_bar.jsx'
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+           <SeachBar />
+         </div>
+      );
+   }
+}
+
+export default App;
